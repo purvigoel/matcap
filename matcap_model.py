d5c1e63166d1b03115b0e08d16f9041fd7169d0f
purvigoel <purvi_goel@brown.edu>
2020-05-17 22:16:41 -0700

    Add files via upload

---
+++ matcap_model.py
@@ -0,0 +1,61 @@
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+image_size = 64
+
+# Number of channels in the training images. For color images this is 3
+nc = 3
+
+# Size of z latent vector (i.e. size of generator input)
+nz = 768 * 2
+
+# Size of feature maps in generator
+ngf = 64
+
+# Size of feature maps in discriminator
+ndf = 64
+
+# Number of GPUs available. Use 0 for CPU mode.
+ngpu = 1
+
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+def get_generator():
+	netG = Generator(ngpu).to(device)
+	netG.load_state_dict(torch.load("generator_64.pth"))  
+	return netG
---
+++ runway_model.py
@@ -0,0 +1,58 @@
+import runway
+from runway.data_types import number, text, image
+from PIL import Image
+import matcap_model 
+import torch
+import numpy
+
+@runway.setup
+def setup():
+    msg = '[SETUP] Running Model'
+    print(msg)
+    model = matcap_model.get_generator()
+    return model
+
+# Every model needs to have at least one command. Every command allows to send
+# inputs and process outputs. To see a complete list of supported inputs and
+# outputs data types: https://sdk.runwayml.com/en/latest/data_types.html
+@runway.command(name='generate',
+                inputs={ 'patch': image(width=16, height=16) },
+                outputs={ 'image': image(width=64, height=64) })
+def generate(model, args):
+    # Generate a PIL or Numpy image based on the input caption, and return it
+    img = args['patch']
+    img_tensor  = torch.tensor(numpy.array(img)).float() 
+    
+    noise = torch.stack([img_tensor, img_tensor, img_tensor, img_tensor, img_tensor,
+        img_tensor, img_tensor, img_tensor, img_tensor, img_tensor,
+        img_tensor, img_tensor, img_tensor, img_tensor, img_tensor])
+    noise = noise.view(15, -1).float() 
+
+    input_image = Image.open("./input.png")
+    input_image = numpy.array(input_image.resize((16, 16)))
+
+    input_images = []
+
+    for inp in range(3 * 5):
+      input_images.append(input_image)
+    input_images = torch.tensor(input_images).float()
+
+
+    inp = input_images.view(15, -1)
+    noise = torch.cat((noise, inp),dim=1).unsqueeze(0).unsqueeze(0).permute(2, 3, 0 ,1)
+
+
+    output_image = model(noise)[2]
+    output_image = output_image.clamp(min=-1, max=1)
+    output_image = (output_image - output_image.min())/(output_image.max() - output_image.min())
+    # output_image = ((output_image + 1.0) * 255 / 2.0)
+    output_image = output_image.permute(1,2,0) * 255.0
+    output_image = output_image.detach().numpy().astype(numpy.uint8)
+    return {
+        'image': output_image
+    }
+
+if __name__ == '__main__':
+    # run the model server using the default network interface and ports,
+    # displayed here for convenience
+    runway.run(host='0.0.0.0', port=8000)
